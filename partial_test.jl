0ff8c85316694a80bcccd1363d8a372b74c6eccb
Priscilla (Laila Sandra) <a.g.marosi@bluewin.ch>
2025-05-03 18:30:20 +0200

    stuff

--- partial_test.jl
+++ partial_test.jl
@@ -2,6 +2,9 @@ using Format
 
 include("partial.jl")
 
+x, y = -1, 1
+x⃗ = [x, y]
+
 f(x, y) = 3x * y^3 + 10x^2 * y + 5y + 3y * sin(5x * y)
 ∂xf(x, y) = 3y^3 + 20x * y + 3y * cos(5x * y) * 5y
 ∂yf(x, y) = 3x * 3y^2 + 10x^2 + 5 + (3sin(5x * y) + 3y * cos(5x * y) * 5x)
@@ -11,5 +14,5 @@ function g(input)
   return f(x, y)
 end
 
-printfmtln("{:.2e}", abs(∂xf(-1, 1) - ∂1_4(g, [-1, 1], 1)))
-printfmtln("{:.2e}", abs(∂yf(-1, 1) - ∂1_4(g, [-1, 1], 2)))
+printfmtln("{:.2e}", abs(∂xf(x, y) - ∂1_4(g, x⃗, 1)))
+printfmtln("{:.2e}", abs(∂yf(x, y) - ∂1_4(g, x⃗, 2)))
