a5c7dd832ea1a5a4fd3a283aa67360aac278057c
Priscilla (Laila Sandra) <a.g.marosi@bluewin.ch>
2025-05-16 11:26:44 +0200

    Added small testing code

---
+++ test.jl
@@ -0,0 +1,105 @@
+using Printf
+
+module A
+
+function d(f::Function, x, h=0.001)
+  # return ((1 / 12)f(x - 2h) - (2 / 3)f(x - h) + (2 / 3)f(x + h) - (1 / 12)f(x + 2h)) / h
+
+  # Even this implementation seems to be more accurate than the direct formulas?
+  return (-f(x - h) + f(x + h)) / 2h
+end
+
+function d2(f::Function, x, h=0.001)
+  return d(x -> d(f, x, h), x)
+end
+
+function d3(f::Function, x, h=0.001)
+  return d(x -> d2(f, x, h), x)
+end
+
+function d4(f::Function, x, h=0.001)
+  return d(x -> d3(f, x, h), x)
+end
+
+end
+
+module B
+
+function d4_4(f::Function, x, h::Real=1e-3)
+  return (
+    -(1 / 6)f(x - 3h) + 2f(x - 2h) - (13 / 2)f(x - h) + (28 / 3)f(x) - (13 / 2)f(x + h) +
+    2f(x + 2h) - (1 / 6)f(x + 3h)
+  ) / h^4
+end
+
+function d4_6(f::Function, x, h::Real=1e-3)
+  return (
+    (7 / 240)f(x - 4h) - 0.4f(x - 3h) + (169 / 60)f(x - 2h) - (122 / 15)f(x - h) + 11.375f(x) -
+    (122 / 15)f(x + h) + (169 / 60)f(x + 2h) - 0.4f(x + 3h) + (7 / 240)f(x + 4h)
+  ) / h^4
+end
+
+end
+
+f1(x) = x^3
+f2(x) = 1 / x
+f3(x) = sqrt(x)
+f4(x) = exp(x)
+f5(x) = log(x)
+
+d4_f1(x) = 0
+d4_f2(x) = 24 / x^5
+d4_f3(x) = -(15 / 16x^(7 / 2))
+d4_f4(x) = exp(x)
+d4_f5(x) = -(6 / x^4)
+
+# Todo: scoring
+A_score = 0
+B_score = 0
+
+for x in [1, 2, 5, 10, 20, 50, 100, 200, 500]
+  println("  x = $x")
+  println("----- Module A -----")
+  @printf("f1 ε = %.2e\n", abs(d4_f1(x) - A.d4(f1, x)))
+  @printf("f2 ε = %.2e\n", abs(d4_f2(x) - A.d4(f2, x)))
+  @printf("f3 ε = %.2e\n", abs(d4_f3(x) - A.d4(f3, x)))
+  @printf("f4 ε = %.2e\n", abs(d4_f4(x) - A.d4(f4, x)))
+  @printf("f5 ε = %.2e\n", abs(d4_f5(x) - A.d4(f5, x)))
+
+  println("----- Module B (accuracy 4) -----")
+  @printf("f1 ε = %.2e\n", abs(d4_f1(x) - B.d4_4(f1, x)))
+  @printf("f2 ε = %.2e\n", abs(d4_f2(x) - B.d4_4(f2, x)))
+  @printf("f3 ε = %.2e\n", abs(d4_f3(x) - B.d4_4(f3, x)))
+  @printf("f4 ε = %.2e\n", abs(d4_f4(x) - B.d4_4(f4, x)))
+  @printf("f5 ε = %.2e\n", abs(d4_f5(x) - B.d4_4(f5, x)))
+
+  println("----- Module B (accuracy 6) -----")
+  @printf("f1 ε = %.2e\n", abs(d4_f1(x) - B.d4_6(f1, x)))
+  @printf("f2 ε = %.2e\n", abs(d4_f2(x) - B.d4_6(f2, x)))
+  @printf("f3 ε = %.2e\n", abs(d4_f3(x) - B.d4_6(f3, x)))
+  @printf("f4 ε = %.2e\n", abs(d4_f4(x) - B.d4_6(f4, x)))
+  @printf("f5 ε = %.2e\n", abs(d4_f5(x) - B.d4_6(f5, x)))
+  println()
+end
+
+#=
+# Todo: better performance tests
+println("----- nested functions -----")
+@time A.d4(f1, x)
+@time A.d4(f2, x)
+@time A.d4(f3, x)
+@time A.d4(f4, x)
+@time A.d4(f5, x)
+println("----- direct formula (accuracy 4) -----")
+@time B.d4_4(f1, x)
+@time B.d4_4(f2, x)
+@time B.d4_4(f3, x)
+@time B.d4_4(f4, x)
+@time B.d4_4(f5, x)
+println("----- direct formula (accuracy 6) -----")
+@time B.d4_6(f1, x)
+@time B.d4_6(f2, x)
+@time B.d4_6(f3, x)
+@time B.d4_6(f4, x)
+@time B.d4_6(f5, x)
+=#
